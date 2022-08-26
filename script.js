017979676ac5d2760dd11981f6f559e2ab11a0f3
osmanyusufakkus <osmanyusufakkus@gmail.com>
2022-08-26 20:40:25 +0300

    Update

--- script.js
+++ script.js
@@ -19,41 +19,62 @@ const score1Current = document.querySelector('#current--1');
 const overlay0 = document.querySelector('.player--0');
 const overlay1 = document.querySelector('.player--1');
 
-console.log(score0Current);
-
 score0.textContent = 0;
 score1.textContent = 0;
 
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayers = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  overlay0.classList.toggle('player--active');
+  overlay1.classList.toggle('player--active');
+};
 
 dice.classList.add('hidden');
 
 btnNew.addEventListener('click', function () {
+  playing = true;
   dice.classList.add('hidden');
 });
 
 btnRoll.addEventListener('click', function () {
-  const diceNum = randomNum(1, 6);
-  dice.classList.remove('hidden');
-  dice.src = `dice-${diceNum}.png`;
-  if (diceNum == 1) {
-    console.log('sıra diğer oyuncuda');
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    overlay0.classList.toggle('player--active');
-    overlay1.classList.toggle('player--active');
-  } else {
-    console.log('puan toplamaya devam');
-    currentScore += diceNum;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+  if (playing) {
+    const diceNum = randomNum(1, 6);
+    dice.classList.remove('hidden');
+    dice.src = `dice-${diceNum}.png`;
+    if (diceNum == 1) {
+      console.log('sıra diğer oyuncuda');
+      switchPlayers();
+    } else {
+      console.log('puan toplamaya devam');
+      currentScore += diceNum;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    }
   }
 });
-/*
+
 btnHold.addEventListener('click', function () {
-  score0.textContent += currentScore;
-  currentScore = 0;
-});*/
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 10) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      dice.classList.add('hidden');
+    } else {
+      switchPlayers();
+    }
+  }
+});
